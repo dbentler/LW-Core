1f61688c4811bec6410e9c2ce9a75328c0c357bb
BuildTools <unconfigured@null.spigotmc.org>
2019-07-14 01:41:12 -0500

    Fixed a build tool patch error, fixed faction fly glitch.

--- src/me/ezjamo/managers/WildToolsFixManager.java
+++ src/me/ezjamo/managers/WildToolsFixManager.java
@@ -1,6 +1,7 @@
 package me.ezjamo.managers;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -12,15 +13,15 @@ public class WildToolsFixManager implements Listener {
 
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent e) {
-		if (!e.getPlayer().getInventory().getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&', "&f&lLone&4&lWolves &f&lBuild Wand")))	{	
+		if (!e.getPlayer().getItemInHand().hasItemMeta() && (!e.getPlayer().getInventory().getItemInHand().getType().equals(Material.GOLD_AXE)))
 			return;
-		}	
+
 		FPlayer fPlayer = FPlayers.getInstance().getByPlayer(e.getPlayer());
-		if (e.getPlayer().getInventory().getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&', "&f&lLone&4&lWolves &f&lBuild Wand"))) {
+		if (e.getPlayer().getItemInHand().hasItemMeta() && e.getPlayer().getInventory().getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&', "&f&lLone&4&lWolves &f&lBuild Wand"))) {
 			if (!fPlayer.isInOwnTerritory()) {
 				e.setCancelled(true);
 				e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&l[&f&lLone&4&lWolves&8&l] &fYou can only use build wands in your own claim."));
-						}	
+					}	
 				}
 		}
 }
