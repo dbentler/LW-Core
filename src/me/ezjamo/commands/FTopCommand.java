b354e6c20669d2baf5c1b6791c7ba06acb6217cf
DeathRealms <justintoolove@gmail.com>
2019-07-18 23:44:31 -0400

    Added ftop command

--- src/me/ezjamo/commands/FTopCommand.java
+++ src/me/ezjamo/commands/FTopCommand.java
@@ -3,12 +3,20 @@ package me.ezjamo.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class FTopCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		
+		Player player = (Player) sender;
+		if (label.equalsIgnoreCase("ftop")) {
+		if (args.length == 1) {
+		if (args[0].equalsIgnoreCase("update")) {
+			player.performCommand("factiontop update");
+		}
+		}
+		}
 		return false;
 	}
 
