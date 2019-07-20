da15a3af3e50346db7a3b24e4505d35c480cd213
DeathRealms <justintoolove@gmail.com>
2019-07-19 22:58:05 -0400

    Fixed sponges and added /espawn

---
+++ src/me/ezjamo/commands/ESpawnCommand.java
@@ -0,0 +1,19 @@
+package me.ezjamo.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class ESpawnCommand implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		Player player = (Player) sender;
+		if(cmd.getName().equalsIgnoreCase("espawn")) {
+        	player.performCommand("ewarp spawn");
+        }
+		return false;
+	}
+
+}
