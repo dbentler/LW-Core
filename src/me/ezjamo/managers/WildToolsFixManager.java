4d20c15b2d235b50f25114d054c7c9f0db60cd91
DeathRealms <justintoolove@gmail.com>
2019-08-09 03:22:31 -0400

    Fixed error with wildtools fix.

--- src/me/ezjamo/managers/WildToolsFixManager.java
+++ src/me/ezjamo/managers/WildToolsFixManager.java
@@ -4,7 +4,9 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 import com.massivecraft.factions.FPlayer;
 import com.massivecraft.factions.FPlayers;
@@ -13,12 +15,10 @@ public class WildToolsFixManager implements Listener {
 
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent e) {
-		if (e.getPlayer().getItemInHand().getEnchantments().size() == 0) {
-			if (!e.getPlayer().getItemInHand().hasItemMeta() && (!e.getPlayer().getInventory().getItemInHand().getType().equals(Material.GOLD_AXE)))
-				return;
-			
-			FPlayer fPlayer = FPlayers.getInstance().getByPlayer(e.getPlayer());
-			if (e.getPlayer().getInventory().getItemInHand().getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&', "&f&lLone&4&lWolves &f&lBuild Wand"))) {
+		FPlayer fPlayer = FPlayers.getInstance().getByPlayer(e.getPlayer());
+		ItemStack i = e.getPlayer().getItemInHand();
+		if (e.getAction() == Action.RIGHT_CLICK_BLOCK && i.getType() == Material.GOLD_AXE && i.getItemMeta().hasDisplayName()) {
+			if (i.getItemMeta().getDisplayName().contains("LoneWolves Build Wand")) {
 				if (!fPlayer.isInOwnTerritory()) {
 					e.setCancelled(true);
 					e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&l[&f&lLone&4&lWolves&8&l] &fYou can only use build wands in your own claim."));
