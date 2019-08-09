f0a0e3574b5e1a9af255dad4ab1a95a183e865d3
DeathRealms <justintoolove@gmail.com>
2019-08-09 09:51:36 -0400

    Fixed message not showing when using wands in other claims.

--- src/me/ezjamo/managers/WildToolsFixManager.java
+++ src/me/ezjamo/managers/WildToolsFixManager.java
@@ -18,7 +18,7 @@ public class WildToolsFixManager implements Listener {
 		FPlayer fPlayer = FPlayers.getInstance().getByPlayer(e.getPlayer());
 		ItemStack i = e.getPlayer().getItemInHand();
 		if (e.getAction() == Action.RIGHT_CLICK_BLOCK && i.getType() == Material.GOLD_AXE && i.getItemMeta().hasDisplayName()) {
-			if (i.getItemMeta().getDisplayName().contains("LoneWolves Build Wand")) {
+			if (i.getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&', "&f&lLone&4&lWolves &f&lBuild Wand"))) {
 				if (!fPlayer.isInOwnTerritory()) {
 					e.setCancelled(true);
 					e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&l[&f&lLone&4&lWolves&8&l] &fYou can only use build wands in your own claim."));
@@ -26,7 +26,7 @@ public class WildToolsFixManager implements Listener {
 			}
 		}
 		if (e.getAction() == Action.RIGHT_CLICK_BLOCK && i.getType() == Material.SHEARS && i.getItemMeta().hasDisplayName()) {
-			if (i.getItemMeta().getDisplayName().contains("LoneWolves SellWand")) {
+			if (i.getItemMeta().getDisplayName().equals(ChatColor.translateAlternateColorCodes('&', "&f&lLone&4&lWolves &f&lSell&4&lWand"))) {
 				if (!fPlayer.isInOwnTerritory()) {
 					e.setCancelled(true);
 					e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&l[&f&lLone&4&lWolves&8&l] &fYou can only use sell wands in your own claim."));
