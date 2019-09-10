31c08821f21ddb51e32821fc03bcc859674bf534
DeathRealms <justintoolove@gmail.com>
2019-09-10 15:05:37 -0400

    Added config option for disabling depth strider.

--- src/me/ezjamo/managers/DepthStriderManager.java
+++ src/me/ezjamo/managers/DepthStriderManager.java
@@ -6,17 +6,24 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
+import me.ezjamo.Lonewolves;
 import me.ezjamo.armorequipevent.ArmorEquipEvent;
 
 public class DepthStriderManager implements Listener {
 	
 	@EventHandler
 	public void armorEquipEvent(ArmorEquipEvent e) {
-		if (e.getNewArmorPiece() != null 
-				&& e.getNewArmorPiece().getType() != Material.AIR 
-				&& e.getNewArmorPiece().containsEnchantment(Enchantment.DEPTH_STRIDER)) {
-			e.getNewArmorPiece().removeEnchantment(Enchantment.DEPTH_STRIDER);
-			e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8&l[&f&lLone&4&lWolves&8&l] &fDepth Strider has been removed from your boots because its currently &cdisabled."));
+		boolean enabled = Lonewolves.plugin.getConfig().getBoolean("disable-depth-strider");
+		if (enabled) {
+			if (e.getNewArmorPiece() != null 
+					&& e.getNewArmorPiece().getType() != Material.AIR
+					&& e.getNewArmorPiece().containsEnchantment(Enchantment.DEPTH_STRIDER)) {
+				e.getNewArmorPiece().removeEnchantment(Enchantment.DEPTH_STRIDER);
+				e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', "&8[&f&lLone&4&lWolves&8] &fDepth Strider has been removed from your boots because it's currently &cdisabled."));
+			}
+			if (!enabled) {
+				return;
 			}
 		}
 	}
+}
