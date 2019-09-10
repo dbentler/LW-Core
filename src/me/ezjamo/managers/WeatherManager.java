2db0c32b0394291f745f84412a0c0c382d305304
DeathRealms <justintoolove@gmail.com>
2019-09-09 22:48:15 -0400

    Added config option for disabling weather.

--- src/me/ezjamo/managers/WeatherManager.java
+++ src/me/ezjamo/managers/WeatherManager.java
@@ -4,10 +4,19 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.weather.WeatherChangeEvent;
 
+import me.ezjamo.Lonewolves;
+
 public class WeatherManager implements Listener {
 	
 	@EventHandler
-	public void onWeatherChange(WeatherChangeEvent e) {
-		e.setCancelled(true);
+	public void onWeatherChange(WeatherChangeEvent event) {
+		boolean enabled = Lonewolves.plugin.getConfig().getBoolean("disable-weather");
+		if (enabled) {
+			event.getWorld().setWeatherDuration(0);
+			event.setCancelled(true);
+		}
+		if (!enabled) {
+			return;
+		}
 	}
 }
