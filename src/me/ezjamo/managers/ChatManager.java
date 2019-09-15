b91773d673a1f85980ac331ecdabca54ff11bb37
DeathRealms <justintoolove@gmail.com>
2019-09-15 04:52:45 -0400

    Added a custom chat format.

--- src/me/ezjamo/managers/ChatManager.java
+++ src/me/ezjamo/managers/ChatManager.java
@@ -12,38 +12,39 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import com.massivecraft.factions.FPlayer;
+import com.massivecraft.factions.FPlayers;
+
 import me.clip.placeholderapi.PlaceholderAPI;
 import me.ezjamo.Lonewolves;
 import me.ezjamo.Messages;
 import me.ezjamo.Utils;
 import net.md_5.bungee.api.ChatColor;
 
-public class ChatManager implements Listener
-{
-    public static Set<UUID> staffChat;
-    public static Set<UUID> adminChat;
-    
-    static {
-        ChatManager.staffChat = new HashSet<UUID>();
-    }
-    
-    public static Set<UUID> getStaffChat() {
-        return ChatManager.staffChat;
-    }
-    
-    @EventHandler
-    public void onStaffChat(AsyncPlayerChatEvent event) {
-        final Player player = event.getPlayer();
-        if (ChatManager.staffChat.contains(player.getUniqueId())) {
-            event.setCancelled(true);
-            for (Player staff : Bukkit.getServer().getOnlinePlayers()) {
-                if (staff.hasPermission("lw.staffchat")) {
-                staff.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9[StaffChat] &e" + player.getName() +  ": &c" + event.getMessage()));
-                }
-            }
-        }
-    }
-    
+public class ChatManager implements Listener {
+	public static Set<UUID> staffChat;
+	public static Set<UUID> adminChat;
+
+	static {
+		ChatManager.staffChat = new HashSet<UUID>();
+	}
+
+	public static Set<UUID> getStaffChat() {
+		return ChatManager.staffChat;
+	}
+
+	@EventHandler
+	public void onStaffChat(AsyncPlayerChatEvent event) {
+		final Player player = event.getPlayer();
+		if (ChatManager.staffChat.contains(player.getUniqueId())) {
+			event.setCancelled(true);
+			for (Player staff : Bukkit.getServer().getOnlinePlayers()) {
+				if (staff.hasPermission("lw.staffchat")) {
+					staff.sendMessage(ChatColor.translateAlternateColorCodes('&', "&9[StaffChat] &e" + player.getName() +  ": &c" + event.getMessage()));
+				}
+			}
+		}
+	}
 
 	@EventHandler
 	public void onPlayerJoin(PlayerJoinEvent event) {
@@ -56,7 +57,7 @@ public class ChatManager implements Listener
 			event.setJoinMessage(Utils.msg(placeholders));
 		}
 	}
-	
+
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent event) {
 		String quit = Lonewolves.plugin.getConfig().getString("quit-message");
@@ -68,15 +69,30 @@ public class ChatManager implements Listener
 			event.setQuitMessage(Utils.msg(placeholders));
 		}
 	}
-	
-    @EventHandler
-    public void onChat(AsyncPlayerChatEvent event) {
-    	Player player = (Player) event.getPlayer();
-    	for (String blockedwords : BlockedWordsManager.getManager().getConfig().getStringList("Blocked Words")) {
-    		if (event.getMessage().equalsIgnoreCase(blockedwords)) {
-    			event.setCancelled(true);
-    			player.sendMessage(ChatColor.translateAlternateColorCodes('&', Messages.prefix + "&fYou have said a blacklisted word."));
-    		}
-    	}
-    }
+
+	@EventHandler
+	public void onChat(AsyncPlayerChatEvent event) {
+		Player player = (Player) event.getPlayer();
+		FPlayer fPlayer = FPlayers.getInstance().getByPlayer(player);
+		for (String blockedwords : BlockedWordsManager.getManager().getConfig().getStringList("Blocked Words")) {
+			if (event.getMessage().equalsIgnoreCase(blockedwords)) {
+				event.setCancelled(true);
+				player.sendMessage(ChatColor.translateAlternateColorCodes('&', Messages.prefix + "&fYou have said a blacklisted word."));
+			}
+		}
+		boolean enabled = Lonewolves.plugin.getConfig().getBoolean("chat-format.enabled");
+		if (enabled) {
+			String withFaction = Lonewolves.plugin.getConfig().getString("chat-format.format");
+			String setFaction = PlaceholderAPI.setPlaceholders(event.getPlayer(), withFaction);
+			String withoutFaction = Lonewolves.plugin.getConfig().getString("chat-format.format-no-faction");
+			String setNoFaction = PlaceholderAPI.setPlaceholders(event.getPlayer(), withoutFaction);
+			if (!fPlayer.hasFaction()) {
+				event.setFormat(Utils.msg(setNoFaction.replaceAll("%message%", event.getMessage())));
+			}
+			if (fPlayer.hasFaction()) {
+				event.setFormat(Utils.msg(setFaction.replaceAll("%message%", event.getMessage())));
+			}
+		}
+		else return;
+	}
 }
