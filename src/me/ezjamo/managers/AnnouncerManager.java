e17b7c3adf5bdac54faa4a9124263d3bc83a5768
DeathRealms <justintoolove@gmail.com>
2019-09-21 09:34:26 -0400

    Updated /ma command.

--- src/me/ezjamo/managers/AnnouncerManager.java
+++ src/me/ezjamo/managers/AnnouncerManager.java
@@ -67,7 +67,7 @@ public class AnnouncerManager extends Utils implements Listener, CommandExecutor
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (cmd.getName().equalsIgnoreCase("ma")) {
-			if (args.length != 1) {
+			if (args.length != 1 || !args[0].equalsIgnoreCase("reload")) {
 				message(sender, "&cUsage: &7/ma reload");
 				return true;
 			}
