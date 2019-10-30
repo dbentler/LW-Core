3af7552931b4611e04290ae2859af2a330220080
DeathRealms <justintoolove@gmail.com>
2019-10-30 15:58:45 -0400

    Added click and hover events to chat.

--- src/me/ezjamo/managers/ChatManager.java
+++ src/me/ezjamo/managers/ChatManager.java
@@ -8,6 +8,10 @@ import me.clip.placeholderapi.PlaceholderAPI;
 import me.ezjamo.Lonewolves;
 import me.ezjamo.Messages;
 import me.ezjamo.Utils;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -70,205 +74,114 @@ public class ChatManager extends Utils implements Listener {
 
 	@EventHandler
 	public void onChat(AsyncPlayerChatEvent event) {
+		event.setCancelled(true);
 		Essentials ess = (Essentials) Bukkit.getPluginManager().getPlugin("Essentials");
 		Player player = event.getPlayer();
 		FPlayer fPlayer = FPlayers.getInstance().getByPlayer(player);
-		for (String blockedwords : BlockedWordsManager.getManager().getConfig().getStringList("Blocked Words")) {
-			if (event.getMessage().toLowerCase().contains(blockedwords)) {
-				event.setCancelled(true);
+		User user = ess.getUser(player);
+		boolean enabled = Lonewolves.plugin.getConfig().getBoolean("chat-format.enabled");
+		for (String blockedWords : BlockedWordsManager.getManager().getConfig().getStringList("Blocked Words")) {
+			if (event.getMessage().toLowerCase().contains(blockedWords)) {
 				message(player, Messages.prefix + Messages.playerSaidBlockedWord);
 				return;
 			}
 		}
-		boolean enabled = Lonewolves.plugin.getConfig().getBoolean("chat-format.enabled");
-		User user = ess.getUser(player);
 		if (enabled) {
-			String format = Lonewolves.plugin.getConfig().getString("chat-format.format");
-			String setFormat = PlaceholderAPI.setPlaceholders(event.getPlayer(), format);
-			if (!fPlayer.hasFaction()) {
-				String faction = setFormat.replaceFirst("\\[", "").replaceFirst("]", "").replace("FACTION", "");
-				if (!player.hasPermission("lw.color")) {
-					if (event.getMessage().contains("&1")||
-							event.getMessage().contains("&2")||
-							event.getMessage().contains("&3")||
-							event.getMessage().contains("&4")||
-							event.getMessage().contains("&5")||
-							event.getMessage().contains("&6")||
-							event.getMessage().contains("&7")||
-							event.getMessage().contains("&8")||
-							event.getMessage().contains("&9")||
-							event.getMessage().contains("&0")||
-							event.getMessage().contains("&a")||
-							event.getMessage().contains("&e")||
-							event.getMessage().contains("&d")||
-							event.getMessage().contains("&b")||
-							event.getMessage().contains("&c")||
-							event.getMessage().contains("&o")||
-							event.getMessage().contains("&l")||
-							event.getMessage().contains("&n")||
-							event.getMessage().contains("&m")||
-							event.getMessage().contains("&k")) {
-						if (user._getNickname() != null) {
-							event.setFormat(color(faction.replaceFirst("\\[", "").replaceFirst("] ", "")
-									.replace("%player_nickname%", "~" + user._getNickname()) + event.getMessage().replace("&1", "&f")
-									.replace("&2", "&f")
-									.replace("&3", "&f")
-									.replace("&4", "&f")
-									.replace("&5", "&f")
-									.replace("&6", "&f")
-									.replace("&7", "&f")
-									.replace("&8", "&f")
-									.replace("&9", "&f")
-									.replace("&0", "&f")
-									.replace("&a", "&f")
-									.replace("&e", "&f")
-									.replace("&d", "&f")
-									.replace("&b", "&f")
-									.replace("&c", "&f")
-									.replace("&o", "&f")
-									.replace("&l", "&f")
-									.replace("&n", "&f")
-									.replace("&m", "&f")
-									.replace("&k", "&f")).replace("%", "%%"));
-							return;
-						}
-						if (user._getNickname() == null) {
-							event.setFormat(color(faction.replaceFirst("\\[", "").replaceFirst("] ", "")
-									.replace("%player_nickname%", player.getName()) + event.getMessage().replace("&1", "&f")
-									.replace("&2", "&f")
-									.replace("&3", "&f")
-									.replace("&4", "&f")
-									.replace("&5", "&f")
-									.replace("&6", "&f")
-									.replace("&7", "&f")
-									.replace("&8", "&f")
-									.replace("&9", "&f")
-									.replace("&0", "&f")
-									.replace("&a", "&f")
-									.replace("&e", "&f")
-									.replace("&d", "&f")
-									.replace("&b", "&f")
-									.replace("&c", "&f")
-									.replace("&o", "&f")
-									.replace("&l", "&f")
-									.replace("&n", "&f")
-									.replace("&m", "&f")
-									.replace("&k", "&f")).replace("%", "%%"));
-							return;
-						}
-					}
-					else {
-						if (user._getNickname() != null) {
-							event.setFormat(color(faction.replaceFirst("\\[", "").replaceFirst("] ", "")
-									.replace("%player_nickname%", "~" + user._getNickname()) + event.getMessage()).replace("%", "%%"));
-							return;
-						}
-						if (user._getNickname() == null) {
-							event.setFormat(color(faction.replaceFirst("\\[", "").replaceFirst("] ", "")
-									.replace("%player_nickname%", player.getName()) + event.getMessage()).replace("%", "%%"));
-							return;
-						}
-					}
-				}
-				if (user._getNickname() != null) {
-					event.setFormat(color(faction.replaceFirst("\\[", "").replaceFirst("] ", "")
-							.replace("%player_nickname%", "~" + user._getNickname()) + event.getMessage()).replace("%", "%%"));
-					return;
-				}
-				if (user._getNickname() == null) {
-					event.setFormat(color(faction.replaceFirst("\\[", "").replaceFirst("] ", "")
-							.replace("%player_nickname%", player.getName()) + event.getMessage()).replace("%", "%%"));
-					return;
-				}
+			String channelString = Lonewolves.plugin.getConfig().getString("chat-format.channel");
+			String setChannel = PlaceholderAPI.setPlaceholders(player, channelString);
+
+			String prefixString = Lonewolves.plugin.getConfig().getString("chat-format.prefix");
+			String setPrefix = PlaceholderAPI.setPlaceholders(player, prefixString);
+
+			String nameString = Lonewolves.plugin.getConfig().getString("chat-format.name");
+			String setName = PlaceholderAPI.setPlaceholders(player, nameString);
+
+			String suffixString = Lonewolves.plugin.getConfig().getString("chat-format.suffix");
+			String setSuffix = PlaceholderAPI.setPlaceholders(player, suffixString);
+
+			TextComponent base = new TextComponent("");
+			TextComponent channel = new TextComponent(setChannel);
+			TextComponent prefix = new TextComponent(color(setPrefix));
+			TextComponent name = new TextComponent(setName);
+			if (user._getNickname() != null) {
+				name = new TextComponent(color(setName.replace("%player_nickname%", "~" + user._getNickname())));
 			}
-			if (fPlayer.hasFaction()) {
-				if (!player.hasPermission("lw.color")) {
-					if (event.getMessage().contains("&1")||
-							event.getMessage().contains("&2")||
-							event.getMessage().contains("&3")||
-							event.getMessage().contains("&4")||
-							event.getMessage().contains("&5")||
-							event.getMessage().contains("&6")||
-							event.getMessage().contains("&7")||
-							event.getMessage().contains("&8")||
-							event.getMessage().contains("&9")||
-							event.getMessage().contains("&0")||
-							event.getMessage().contains("&a")||
-							event.getMessage().contains("&e")||
-							event.getMessage().contains("&d")||
-							event.getMessage().contains("&b")||
-							event.getMessage().contains("&c")||
-							event.getMessage().contains("&o")||
-							event.getMessage().contains("&l")||
-							event.getMessage().contains("&n")||
-							event.getMessage().contains("&m")||
-							event.getMessage().contains("&k")) {
-						if (user._getNickname() != null) {
-							event.setFormat(color(setFormat.replace("%player_nickname%", "~" + user._getNickname()) + event.getMessage().replace("&1", "&f")
-									.replace("&2", "&f")
-									.replace("&3", "&f")
-									.replace("&4", "&f")
-									.replace("&5", "&f")
-									.replace("&6", "&f")
-									.replace("&7", "&f")
-									.replace("&8", "&f")
-									.replace("&9", "&f")
-									.replace("&0", "&f")
-									.replace("&a", "&f")
-									.replace("&e", "&f")
-									.replace("&d", "&f")
-									.replace("&b", "&f")
-									.replace("&c", "&f")
-									.replace("&o", "&f")
-									.replace("&l", "&f")
-									.replace("&n", "&f")
-									.replace("&m", "&f")
-									.replace("&k", "&f")).replace("%", "%%"));
-							return;
-						}
-						if (user._getNickname() == null) {
-							event.setFormat(color(setFormat.replace("%player_nickname%", player.getName()) + event.getMessage().replace("&1", "&f")
-									.replace("&2", "&f")
-									.replace("&3", "&f")
-									.replace("&4", "&f")
-									.replace("&5", "&f")
-									.replace("&6", "&f")
-									.replace("&7", "&f")
-									.replace("&8", "&f")
-									.replace("&9", "&f")
-									.replace("&0", "&f")
-									.replace("&a", "&f")
-									.replace("&e", "&f")
-									.replace("&d", "&f")
-									.replace("&b", "&f")
-									.replace("&c", "&f")
-									.replace("&o", "&f")
-									.replace("&l", "&f")
-									.replace("&n", "&f")
-									.replace("&m", "&f")
-									.replace("&k", "&f")).replace("%", "%%"));
-							return;
-						}
-					}
-					else {
-						if (user._getNickname() != null) {
-							event.setFormat(color(setFormat.replace("%player_nickname%", "~" + user._getNickname()) + event.getMessage()).replace("%", "%%"));
-							return;
-						}
-						if (user._getNickname() == null) {
-							event.setFormat(color(setFormat.replace("%player_nickname%", player.getName()) + event.getMessage()).replace("%", "%%"));
-							return;
-						}
-					}
-				}
-				if (user._getNickname() != null) {
-					event.setFormat(color(setFormat.replace("%player_nickname%", "~" + user._getNickname()) + event.getMessage()).replace("%", "%%"));
-					return;
-				}
-				if (user._getNickname() == null) {
-					event.setFormat(color(setFormat.replace("%player_nickname%", player.getName()) + event.getMessage()).replace("%", "%%"));
-				}
+			if (user._getNickname() == null) {
+				name = new TextComponent(color(setName.replace("%player_nickname%", player.getName())));
 			}
+			TextComponent suffix = new TextComponent(color(setSuffix));
+			TextComponent message = new TextComponent("");
+			if (player.hasPermission("lw.color")) {
+				message = new TextComponent(color(event.getMessage()));
+			}
+			if (!player.hasPermission("lw.color")) {
+				message = new TextComponent(event.getMessage());
+			}
+
+			// Channel
+			for (String channelHover : Lonewolves.plugin.getConfig().getStringList("chat-format.channel-tooltip")) {
+				String placeholders = PlaceholderAPI.setPlaceholders(player, channelHover);
+				BaseComponent[] text = TextComponent.fromLegacyText(color(placeholders.replace("|", "\n")));
+				channel.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, text));
+			}
+			String channelClick = Lonewolves.plugin.getConfig().getString("chat-format.channel-click-command");
+			String channelPlaceholders = PlaceholderAPI.setPlaceholders(player, channelClick);
+			channel.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, channelPlaceholders));
+			// Channel
+
+			// Prefix
+			for (String prefixHover : Lonewolves.plugin.getConfig().getStringList("chat-format.prefix-tooltip")) {
+				String placeholders = PlaceholderAPI.setPlaceholders(player, prefixHover);
+				BaseComponent[] text = TextComponent.fromLegacyText(color(placeholders.replace("|", "\n")));
+				prefix.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, text));
+			}
+			String prefixClick = Lonewolves.plugin.getConfig().getString("chat-format.prefix-click-command");
+			String prefixPlaceholders = PlaceholderAPI.setPlaceholders(player, prefixClick);
+			prefix.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, prefixPlaceholders));
+			// Prefix
+
+			// Name
+			for (String nameHover : Lonewolves.plugin.getConfig().getStringList("chat-format.name-tooltip")) {
+				String placeholders = PlaceholderAPI.setPlaceholders(player, nameHover);
+				BaseComponent[] text = TextComponent.fromLegacyText(color(placeholders.replace("|", "\n")));
+				name.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, text));
+			}
+			String nameClick = Lonewolves.plugin.getConfig().getString("chat-format.name-click-command");
+			String namePlaceholders = PlaceholderAPI.setPlaceholders(player, nameClick);
+			name.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, namePlaceholders));
+			// Name
+
+			// Suffix
+			for (String suffixHover : Lonewolves.plugin.getConfig().getStringList("chat-format.suffix-tooltip")) {
+				String placeholders = PlaceholderAPI.setPlaceholders(player, suffixHover);
+				BaseComponent[] text = TextComponent.fromLegacyText(color(placeholders.replace("|", "\n")));
+				suffix.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, text));
+			}
+			String suffixClick = Lonewolves.plugin.getConfig().getString("chat-format.suffix-click-command");
+			String suffixPlaceholders = PlaceholderAPI.setPlaceholders(player, suffixClick);
+			suffix.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, suffixPlaceholders));
+			// Suffix
+
+			base.addExtra(prefix);
+			base.addExtra(name);
+			base.addExtra(suffix);
+			base.addExtra(message);
+			for (Player listeningPlayer : event.getRecipients()) {
+				FPlayer you = FPlayers.getInstance().getByPlayer(listeningPlayer);
+				channel = new TextComponent(color(setChannel.replace("[FACTION]", fPlayer.getChatTag(you))));
+				if (!fPlayer.hasFaction()) {
+					channel = new TextComponent(color(setChannel.replaceFirst("\\[", "").replace("FACTION", "")
+							.replaceFirst("]", "").replaceFirst("\\[", "").replaceFirst("] ", "").replaceFirst("]", "")));
+				}
+				for (String channelHover : Lonewolves.plugin.getConfig().getStringList("chat-format.channel-tooltip")) {
+					String placeholders = PlaceholderAPI.setPlaceholders(player, channelHover);
+					BaseComponent[] text = TextComponent.fromLegacyText(color(placeholders.replace("|", "\n")));
+					channel.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, text));
+				}
+				channel.setClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, channelPlaceholders));
+				listeningPlayer.spigot().sendMessage(channel, base);
+			}
+			Bukkit.getConsoleSender().sendMessage(channel.toLegacyText() + base.toLegacyText());
 		}
 	}
 }
